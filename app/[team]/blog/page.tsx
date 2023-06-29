2047e2b695505bd7bb2dcf59b720e8f2ecbc8c0c
seobbang <seobbangway@naver.com>
2023-06-29 20:48:58 +0900

    fix: react import 문제 해결

---
+++ app/[team]/blog/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BlogPage = () => {
+  return <div>블로그페이지입니다.</div>;
+};
+
+export default BlogPage;
---
+++ app/[team]/dashboard/member/page.tsx
@@ -0,0 +1,5 @@
+const MemberPage = () => {
+  return <div>멤버 관리 페이지입니다.</div>;
+};
+
+export default MemberPage;
