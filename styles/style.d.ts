9415c65a081495bee19016c4de3e016b4e556570
Geun-Oh <kandy1002@naver.com>
2023-07-03 21:43:10 +0900

    config: color theme 임시 적용

--- styles/style.d.ts
+++ styles/style.d.ts
@@ -5,31 +5,54 @@ type GreyStyleType =
   | 'White'
   | 'Basic'
   | 'Black'
-  | 0
-  | 50
-  | 100
-  | 200
-  | 300
-  | 400
-  | 500
-  | 600
-  | 700
-  | 800
-  | 900
-  | 950
-  | 1000;
+  | 'grey0'
+  | 'grey50'
+  | 'grey100'
+  | 'grey200'
+  | 'grey300'
+  | 'grey400'
+  | 'grey500'
+  | 'grey600'
+  | 'grey700'
+  | 'grey800'
+  | 'grey900'
+  | 'grey950'
+  | 'grey1000';
 type TypeSystemType = 'Title' | 'Heading1' | 'Heading2' | 'Body1' | 'Body2' | 'Body3' | 'Caption';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
     PointColors: {
-      [key: PointColorsType]: string;
+      Green: string;
+      DarkGreen: string;
+      Blue: string;
     };
     GreyStyle: {
-      [key: GreyStyleType]: string;
+      White: string;
+      Basic: string;
+      Black: string;
+      Grey0: string;
+      Grey50: string;
+      Grey100: string;
+      Grey200: string;
+      Grey300: string;
+      Grey400: string;
+      Grey500: string;
+      Grey600: string;
+      Grey700: string;
+      Grey800: string;
+      Grey900: string;
+      Grey950: string;
+      Grey1000: string;
     };
     TypeSystem: {
-      [key: TypeSystemType]: SerializedStyles;
+      Title: RuleSet<object>;
+      Heading1: RuleSet<object>;
+      Heading2: RuleSet<object>;
+      Body1: RuleSet<object>;
+      Body2: RuleSet<object>;
+      Body3: RuleSet<object>;
+      Caption: RuleSet<object>;
     };
   }
 }
