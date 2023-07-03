4554ef029923d2cec7b8304b629103a54f57ce1b
seobbang <seobbangway@naver.com>
2023-07-04 03:17:41 +0900

    feat/#1: QueryClient 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 'use client';
 // 팀 블로그 헤더
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from 'styled-components';
 
+import Header from '@/components/common/Header';
 import { GlobalStyle } from '@/styles/GlobalStyle';
 import theme from '@/styles/theme';
 import StyledComponentsRegistry from '@/utils/lib/registry';
@@ -10,15 +12,25 @@ import StyledComponentsRegistry from '@/utils/lib/registry';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 0,
+      },
+    },
+  });
   return (
     <html lang="ko">
       <body className={inter.className}>
-        <StyledComponentsRegistry>
-          <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            {children}
-          </ThemeProvider>
-        </StyledComponentsRegistry>
+        <QueryClientProvider client={queryClient}>
+          <StyledComponentsRegistry>
+            <ThemeProvider theme={theme}>
+              <GlobalStyle />
+              <Header />
+              {children}
+            </ThemeProvider>
+          </StyledComponentsRegistry>
+        </QueryClientProvider>
       </body>
     </html>
   );
