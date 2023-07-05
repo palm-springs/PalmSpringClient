7eb2ec3c18d652f58953a77eafaff5197240cc0f
Geun-Oh <kandy1002@naver.com>
2023-07-05 23:31:20 +0900

    img: 대시보드 navbar 메뉴 아이콘 추가 및 export 설정

---
+++ components/dashboard/components/sideBarContents/index.ts
@@ -0,0 +1,21 @@
+import BlogConfigNavBtn from './BlogConfigNavBtn';
+import BlogDirectNavBtn from './BlogDirectNavBtn';
+import CategoryNavBtn from './CategoryNavBtn';
+import MemberNavBtn from './MemberNavBtn';
+import NavigationNavBtn from './NavigationNavBtn';
+import PageNavBtn from './PageNavBtn';
+import SubscriberNavBtn from './SubscriberNavBtn';
+import TempSavedNavBtn from './TempSavedNavBtn';
+import UploadedNavBtn from './UploadedNavBtn';
+
+export {
+  BlogConfigNavBtn,
+  BlogDirectNavBtn,
+  CategoryNavBtn,
+  MemberNavBtn,
+  NavigationNavBtn,
+  PageNavBtn,
+  SubscriberNavBtn,
+  TempSavedNavBtn,
+  UploadedNavBtn,
+};
