fe15e56f8d4ce689fa880524e1b6ae94c98f0742
Geun-Oh <kandy1002@naver.com>
2023-07-06 01:05:48 +0900

    init/#27:체리픽, 폴더 구조, 세팅

---
+++ app/[team]/editor/article/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ArticleTitle from '@/components/editor/article/ArticleTitle';
+
+const WriteArticle = () => {
+  return (
+    <div>
+      <ArticleTitle />
+    </div>
+  );
+};
+
+export default WriteArticle;
