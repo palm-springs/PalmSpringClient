b7f0ded24203d97b0e529ee26b767e048271ebcb
seobbang <seobbangway@naver.com>
2023-07-06 00:56:40 +0900

    fix/#1: queryClient useState 적용하기

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 'use client';
-// 팀 블로그 헤더
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from 'styled-components';
 
-import Header from '@/components/common/Header';
 import { GlobalStyle } from '@/styles/GlobalStyle';
 import theme from '@/styles/theme';
 import StyledComponentsRegistry from '@/utils/lib/registry';
@@ -12,13 +11,16 @@ import StyledComponentsRegistry from '@/utils/lib/registry';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: 0,
+  const [queryClient] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: 0,
+        },
       },
-    },
-  });
+    }),
+  );
+
   return (
     <html lang="ko">
       <head>
