51bb808a61a32fea99ab4a87853adabdf1a72851
seobbang <seobbangway@naver.com>
2023-07-06 01:40:48 +0900

    feat/#1: getAccessToken 함수 response 타입 지정

--- types/auth.ts
+++ types/auth.ts
@@ -6,3 +6,11 @@ export interface authClientInfo {
   clientId: string;
   clientSecret: string;
 }
+
+export interface getAccessTokenResponse {
+  access_token: string;
+  expires_in: number;
+  token_type: string;
+  scope: string;
+  refresh_token: string;
+}
