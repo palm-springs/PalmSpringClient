095108b0c75d6489f668f14e1e03450db8fb2b0b
Geun-Oh <kandy1002@naver.com>
2023-07-05 23:30:16 +0900

    chore: @svgr/webpack 삭제

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
