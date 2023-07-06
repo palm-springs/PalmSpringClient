d274e5d1c0de3a630848617e9da6f403a4c9d037
MyungJi_Jang <myungvely712@hufs.ac.kr>
2023-07-07 01:50:19 +0900

    feat/#27/title 스타일

--- components/editor/article/ArticleTitle.tsx
+++ components/editor/article/ArticleTitle.tsx
@@ -1,7 +1,51 @@
+'use client';
+
 import React from 'react';
+import styled from 'styled-components';
+
+import {
+  BoldIcon,
+  BulletIcon,
+  CodeBlockIcon,
+  CodeIcon,
+  H1Icon,
+  H2Icon,
+  H3Icon,
+  HorizonIcon,
+  ImageIcon,
+  ItalicIcon,
+  LinkIcon,
+  OrderIcon,
+  QuoteIcon,
+  StrikeIcon,
+} from '@/public/icons';
 
 const ArticleTitle = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Input type="text" placeholder="제목을 입력해주세요" className="input-header" />
+      <div></div>
+    </div>
+  );
 };
 
 export default ArticleTitle;
+
+const Input = styled.input`
+  border: none;
+  background: ${({ theme }) => theme.colors.grey_0};
+  width: 100%;
+  height: 100%;
+  color: ${({ theme }) => theme.colors.grey_900};
+  font-family: ${({ theme }) => theme.fonts.Title};
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.grey_900};
+  }
+  &:focus {
+    outline: none;
+  }
+  /* 포커스시 감출것인가 물어보기 */
+  &:focus::placeholder {
+    color: transparent;
+  }
+`;
