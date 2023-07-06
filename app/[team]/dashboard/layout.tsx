208ac635a15a57b9d3292e000b09467c848b19c8
Geun-Oh <kandy1002@naver.com>
2023-07-06 11:54:55 +0900

    refactor: 페이지 이동 버튼 mapping해서 조건분기하도록 리팩토링

--- app/[team]/dashboard/layout.tsx
+++ app/[team]/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import DashBoardTemplate from '@/components/dashboard/components/DashBoardTemplate';
+import DashBoardTemplate from '@/components/dashboard/DashBoardTemplate';
 
 const DashBoardLayout = ({ children }: { children: React.ReactNode }) => {
   return <DashBoardTemplate>{children}</DashBoardTemplate>;
