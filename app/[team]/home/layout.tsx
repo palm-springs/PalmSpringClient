14e33d766f74f7c97ddeac50c0825cc9c586abd0
SynthiaLee <seanne0801@gmail.com>
2023-07-07 17:12:54 +0900

    feat: 블로그 메인 페이지 레이아웃 및 페이지 구현

--- app/[team]/home/layout.tsx
+++ app/[team]/home/layout.tsx
@@ -1,1 +1,21 @@
-// 대문이미지 + 카테고리 바
+// 헤더 + 대문이미지 + 카테고리 바
+'use client';
+
+import React from 'react';
+
+import BlogImg from '@/components/blog/BlogImg';
+import Footer from '@/components/blog/Footer';
+import BlogHeader from '@/components/blog/UI/BlogHeader';
+
+const BlogHomeLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <BlogHeader />
+      <BlogImg />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default BlogHomeLayout;
