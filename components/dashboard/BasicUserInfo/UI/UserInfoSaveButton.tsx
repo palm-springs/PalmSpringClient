4c5cf21c5856344571a2649f689b0211a2536125
MyungJi_Jang <myungvely712@hufs.ac.kr>
2023-07-09 01:57:29 +0900

    feat/#42/유저 정보 저장 버튼, 블로그, 팜스프링 삭제 탈퇴 버튼

---
+++ components/dashboard/BasicUserInfo/UI/UserInfoSaveButton.tsx
@@ -0,0 +1,25 @@
+'use client';
+import React from 'react';
+import styled from 'styled-components';
+
+const UserInfoSaveButton = () => {
+  return (
+    <>
+      <SaveButton type="button">저장하기</SaveButton>
+    </>
+  );
+};
+
+export default UserInfoSaveButton;
+
+const SaveButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0.8rem;
+  background-color: ${({ theme }) => theme.colors.green};
+  padding: 1rem 2rem;
+  width: 9.6rem;
+  height: 3.6rem;
+  color: ${({ theme }) => theme.colors.grey_0};
+`;
