cf0b82d7718b58c13b385b06fac5bbc8820dbedb
Geun-Oh <kandy1002@naver.com>
2023-07-08 17:31:32 +0900

    feat: 대시보드 관련 스타일 UI 추가

---
+++ components/dashboard/components/ui/DashBoardProfileContainer.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Image from 'next/image';
+import { styled } from 'styled-components';
+
+import { DuckDuckImg } from '@/public/images';
+
+interface DashBoardProfileContainerProps {
+  userName: string;
+  email: string;
+  profileImgUrl?: string;
+}
+
+const DashBoardProfileContainer = (props: DashBoardProfileContainerProps) => {
+  const { userName, email, profileImgUrl } = props;
+
+  return (
+    <DashBoardProfileContainerUI>
+      <Image src={profileImgUrl ?? DuckDuckImg} alt="프로필 이미지" />
+      <div>
+        <span>{userName}</span>
+        <span>{email}</span>
+      </div>
+    </DashBoardProfileContainerUI>
+  );
+};
+
+export default DashBoardProfileContainer;
+
+const DashBoardProfileContainerUI = styled.article`
+  display: flex;
+  align-items: center;
+  width: 25.4rem;
+  height: 5.6rem;
+
+  & > img {
+    margin: 0.8rem;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    :first-child {
+      margin-bottom: 0.2rem;
+      height: 1.7rem;
+      ${({ theme }) => theme.fonts.Body3_Semibold};
+      color: ${({ theme }) => theme.colors.grey_900};
+    }
+    :nth-child(2) {
+      height: 1.7rem;
+      ${({ theme }) => theme.fonts.Body3_Regular};
+      color: ${({ theme }) => theme.colors.grey_700};
+    }
+  }
+  #dashboard_navbar_profile_btn {
+    background: ${({ theme }) => theme.colors.grey_900};
+  }
+`;
