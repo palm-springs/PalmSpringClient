33220123aeb4543bb851b5bc814b7a27345317b1
MyungJi_Jang <myungvely712@hufs.ac.kr>
2023-07-09 01:55:40 +0900

    feat/#42/유저 직책

---
+++ components/dashboard/BasicUserInfo/UI/UserPosition.tsx
@@ -0,0 +1,37 @@
+'user client';
+import React from 'react';
+import styled from 'styled-components';
+
+const UserPosition = () => {
+  return (
+    <UserPositionContainer>
+      <UserPositionTitle>직책</UserPositionTitle>
+      <UserPositionTextarea placeholder="직책을 입력해주세요"></UserPositionTextarea>
+    </UserPositionContainer>
+  );
+};
+
+export default UserPosition;
+
+const UserPositionTextarea = styled.textarea`
+  gap: 1rem;
+  align-items: flex-start;
+  border: 0.1rem solid ${({ theme }) => theme.colors.grey_600};
+  border-radius: 0.8rem;
+  padding: 1rem 1.2rem;
+  width: 50rem;
+  height: 4.6rem;
+  resize: none;
+  ${({ theme }) => theme.fonts.Body2_Regular};
+  color: ${({ theme }) => theme.colors.grey_600};
+`;
+
+const UserPositionContainer = styled.div`
+  margin: 3.2rem 0;
+`;
+
+const UserPositionTitle = styled.p`
+  margin-bottom: 0.8rem;
+  color: ${({ theme }) => theme.colors.grey_950};
+  ${({ theme }) => theme.fonts.Body2_Semibold};
+`;
