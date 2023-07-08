d5145e360701819fcd8f93a14df22ffec6d598cb
Geun-Oh <kandy1002@naver.com>
2023-07-09 03:55:09 +0900

    feat: Nav Button 라우터 추가 및 예외처리

---
+++ components/dashboard/components/ui/NavButtonContainer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+interface NavButtonContainerProps {
+  children: React.ReactNode;
+  onNavButtonClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const NavButtonContainer = (props: NavButtonContainerProps) => {
+  const { children, onNavButtonClick } = props;
+
+  return <NavButtonUI onClick={onNavButtonClick}>{children}</NavButtonUI>;
+};
+
+export default NavButtonContainer;
+
+const NavButtonUI = styled.button`
+  margin-bottom: 0.8rem;
+`;
