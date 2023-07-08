5198ffe57ee1d73d3de1d9ea18ca3a4c49b36d37
SynthiaLee <seanne0801@gmail.com>
2023-07-08 21:41:20 +0900

    feat/#25: 이미지 파일 추가

--- public/images/index.tsx
+++ public/images/index.tsx
@@ -1,2 +1,4 @@
 // 명명규칙은 xxxxImg으로 통일
+export { default as ArticleExampleImg } from './ArticleExample.png';
+export { default as AuthorProfileExampleImg } from './AuthorProfileExample.png';
 export { default as GoogleImg } from './google_icon.png';
