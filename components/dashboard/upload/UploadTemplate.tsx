d4d6c05a491c55d06156faace39f66189738cb14
Geun-Oh <kandy1002@naver.com>
2023-07-11 03:21:09 +0900

    feat: 업로드된 글 퍼블리싱

--- components/dashboard/upload/UploadTemplate.tsx
+++ components/dashboard/upload/UploadTemplate.tsx
@@ -2,19 +2,19 @@
 
 import React from 'react';
 
+import DashBoardTemplateContainer from '../components/ui/DashBoardTemplateContainer';
 import Line from '../components/ui/Line';
 
-import UploadTemplateContainer from './components/ui/UploadTemplateContainer';
 import UploadContentList from './components/UploadContentList';
 import UploadTabBar from './components/UploadTabBar';
 
 const UploadTemplate = () => {
   return (
-    <UploadTemplateContainer>
+    <DashBoardTemplateContainer>
       <UploadTabBar />
       <Line />
       <UploadContentList />
-    </UploadTemplateContainer>
+    </DashBoardTemplateContainer>
   );
 };
 
--- components/dashboard/upload/components/UploadContentList.tsx
+++ components/dashboard/upload/components/UploadContentList.tsx
@@ -1,52 +1,54 @@
+'use client';
+
 import React from 'react';
 
-import UploadContentListContainer from './ui/UploadContentListContainer';
-import UploadContent from './UploadContent';
+import DashBoardContent from '../../components/DashBoardContent';
+import DashBoardContentListContainer from '../../components/ui/DashBoardContentListContainer';
 
 const UploadContentList = () => {
   return (
-    <UploadContentListContainer>
-      <UploadContent
+    <DashBoardContentListContainer>
+      <DashBoardContent
         content="전 세계에서 폭발적으로 인기를 끈 가상 현실 게임,'넥서스' 출시!"
         tabType="문화"
         author="김서윤"
         position="TL"
-        createdAt="2023년 7월 6일"
+        createdAt="2023.07.06"
         onTitleClick={() => {
           console.log('김서윤');
         }}
       />
-      <UploadContent
+      <DashBoardContent
         content="로봇 의료진의 시대,인공지능 로봇 수술로 성공률 급증"
         tabType="개발"
         author="최수빈"
         position="Product Designer"
-        createdAt="2023년 7월 6일"
+        createdAt="2023.07.06"
         onTitleClick={() => {
           console.log('김서윤');
         }}
       />
-      <UploadContent
+      <DashBoardContent
         content="심장 건강을 책임지는 스마트 워치,심박수 감시와 예방 기능 탑재"
         tabType="문화"
         author="이서준"
         position="PO"
-        createdAt="2023년 7월 6일"
+        createdAt="2023.07.06"
         onTitleClick={() => {
           console.log('김서윤');
         }}
       />
-      <UploadContent
+      <DashBoardContent
         content="전 세계에서 폭발적으로 인기를 끈 가상 현실 게임,'넥서스' 출시!"
         tabType="문화"
         author="김서윤"
         position="TL"
-        createdAt="2023년 7월 6일"
+        createdAt="2023.07.06"
         onTitleClick={() => {
           console.log('김서윤');
         }}
       />
-    </UploadContentListContainer>
+    </DashBoardContentListContainer>
   );
 };
 
