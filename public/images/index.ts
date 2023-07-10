51f10f28b43a406090fb8049ae651e1004782487
SynthiaLee <seanne0801@gmail.com>
2023-07-10 13:38:10 +0900

    feat: 블로그 관련 이미지 추가

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,3 +1,5 @@
 // 명명규칙은 xxxxImg으로 통일
+export { default as BlogSampleImg } from './BlogSample.png';
 export { default as GoogleImg } from './google_icon.png';
+export { default as MemberExampleImg } from './memberExample.png';
 export { default as DuckDuckImg } from './Rectangle 65.png';
