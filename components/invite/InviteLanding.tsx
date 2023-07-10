f27b25c781480c146d6e67d4ebe53bde7bd41340
seobbang <seobbangway@naver.com>
2023-07-11 02:43:32 +0900

    feat/#19: 초대 수락 뷰 전체 구현

---
+++ components/invite/InviteLanding.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import InviteAcceptForm from './ui/InviteAcceptForm';
+
+const InviteAcceptLanding = () => {
+  return <InviteAcceptForm />;
+};
+
+export default InviteAcceptLanding;
