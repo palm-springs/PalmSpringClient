7165461e25e030100231e4693c8ffcc8e457b99a
Geun-Oh <kandy1002@naver.com>
2023-07-10 19:08:27 +0900

    Update components/dashboard/components/ui/Line.tsx
    Co-authored-by: SeoHyun Kim <seobbangway@naver.com>

--- components/dashboard/components/ui/Line.tsx
+++ components/dashboard/components/ui/Line.tsx
@@ -10,6 +10,6 @@ export default Line;
 const LineUI = styled.div`
   margin-bottom: 0.8rem;
   background: ${({ theme }) => theme.colors.grey_300};
-  width: calc(100%);
+  width: 100%;
   height: 1px;
 `;
