f38b87781eed2346bdfd211508c1c1c743c867fd
Geun-Oh <kandy1002@naver.com>
2023-07-11 03:20:08 +0900

    refactor: 대시보드 공통 컴포넌트 리팩토링

---
+++ components/dashboard/components/DashBoardContent.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import React, { useState } from 'react';
+
+import DashBoardContentContainer from './ui/DashBoardContentContainer';
+
+export interface DashBoardContentProps {
+  content: string;
+  tabType?: string;
+  author?: string;
+  draft?: boolean; // 페이지에 글의 업로드 완료 or 임시저장 여부를 나타냄
+  position?: string; // 아마 팀별 직무가 팀마다 다를 수 있으므로 나중에 서버에서 타입을 받아올 수 있다면 그거로 지정해줍시다.
+  createdAt: string;
+  onTitleClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const DashBoardContent = (props: DashBoardContentProps) => {
+  const [isPopOverMenuOpen, setIsPopOverMenuOpen] = useState<boolean>(false);
+
+  return (
+    <DashBoardContentContainer
+      contentObject={props}
+      onMeatBallClick={setIsPopOverMenuOpen}
+      isPopOverMenuOpen={isPopOverMenuOpen}
+    />
+  );
+};
+
+export default DashBoardContent;
---
+++ components/dashboard/components/ui/DashBoardContentContainer.tsx
@@ -0,0 +1,120 @@
+'use client';
+
+import React, { Dispatch, SetStateAction } from 'react';
+import { styled } from 'styled-components';
+
+import { CharmMenuMeatballIcon } from '@/public/icons';
+
+import PopOverMenu from '../../upload/components/ui/PopOverMenu';
+import { DashBoardContentProps } from '../DashBoardContent';
+
+import Draft from './Draft';
+
+interface DashBoardContentContainerProps {
+  contentObject: DashBoardContentProps;
+  onMeatBallClick: Dispatch<SetStateAction<boolean>>;
+  isPopOverMenuOpen: boolean;
+}
+
+const DashBoardContentContainer = (props: DashBoardContentContainerProps) => {
+  const {
+    contentObject: { content, tabType, draft, author, position, createdAt, onTitleClick },
+    onMeatBallClick,
+    isPopOverMenuOpen,
+  } = props;
+  // 날짜 포맷팅은 나중에 raw 데이터가 어떻게 날아오는지 확인하고 합시다!
+  return (
+    <DashBoardContentUI>
+      <button id="content" onClick={onTitleClick}>
+        {content}
+      </button>
+      {tabType ? <span id="tabType">{tabType}</span> : <></>}
+      <span id="author">{author ?? ''}</span>
+      <span id="position">{position ?? ''}</span>
+      {draft !== undefined ? <Draft draft={draft} /> : <></>}
+      <span id="createdAt">{createdAt ?? ''}</span>
+      <MenuBtn
+        onClick={() => {
+          onMeatBallClick((prev) => !prev);
+        }}
+      />
+      {isPopOverMenuOpen ? <PopOverMenu /> : <></>}
+    </DashBoardContentUI>
+  );
+};
+
+export default DashBoardContentContainer;
+
+const DashBoardContentUI = styled.article`
+  display: flex;
+  position: relative;
+  align-items: center;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grey_300};
+  width: 100%;
+  height: 5.2rem;
+
+  button {
+    border: none;
+    background: none;
+    cursor: pointer;
+  }
+
+  #content {
+    margin-right: 5vw;
+    width: 40vw;
+    overflow: hidden;
+    text-align: left;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    ${({ theme }) => theme.fonts.Body3_Semibold};
+    color: ${({ theme }) => theme.colors.grey_900};
+  }
+
+  #tabType {
+    display: inline-flex;
+    gap: 1rem;
+    align-items: center;
+    margin-right: 2vw;
+    ${({ theme }) => theme.fonts.Body3_Regular};
+    border: 1px solid ${({ theme }) => theme.colors.grey_400};
+    border-radius: 2rem;
+    padding: 0.4rem 0.8rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  #author {
+    margin-right: 2vw;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    ${({ theme }) => theme.fonts.Body3_Regular};
+    color: ${({ theme }) => theme.colors.grey_900};
+  }
+
+  #position {
+    margin-right: 3vw;
+    width: 8.4rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    ${({ theme }) => theme.fonts.Body3_Regular};
+    color: ${({ theme }) => theme.colors.grey_700};
+  }
+
+  #createdAt {
+    width: 10rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    ${({ theme }) => theme.fonts.Body3_Regular};
+    color: ${({ theme }) => theme.colors.grey_900};
+  }
+`;
+
+const MenuBtn = styled(CharmMenuMeatballIcon)`
+  position: absolute;
+  right: 0;
+  cursor: pointer;
+`;
