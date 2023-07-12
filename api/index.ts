b956ed510daf6b2b7b17bfa0556a64dfa0f055d3
seobbang <seobbangway@naver.com>
2023-07-12 22:35:16 +0900

    feat/#73: axiosInstance 생성

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,6 @@
-// axiosInstance
+import axios, { AxiosInstance } from 'axios';
+
+export const client: AxiosInstance = axios.create({
+  baseURL: process.env.BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+});
