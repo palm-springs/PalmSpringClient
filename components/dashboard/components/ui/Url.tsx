533f4c6e3af29b31d7a2442f2f3f2ee991d7ebaf
Geun-Oh <kandy1002@naver.com>
2023-07-12 10:11:07 +0900

    reflect: 코리 반영

--- components/dashboard/components/ui/Url.tsx
+++ components/dashboard/components/ui/Url.tsx
@@ -14,7 +14,7 @@ const Url = (props: UrlProps) => {
 export default Url;
 
 const UrlUI = styled.span`
-  width: 13rem;
   ${({ theme }) => theme.fonts.Body3_Regular};
+  width: 13rem;
   color: ${({ theme }) => theme.colors.grey_700};
 `;
