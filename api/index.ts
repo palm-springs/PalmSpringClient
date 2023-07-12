23a46e6ff15f7cbf483194d9ac3108e44a5c89cd
seobbang <seobbangway@naver.com>
2023-07-13 01:12:05 +0900

    feat/#73: axiosInstance 생성 및 api 연결 예시

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const client: AxiosInstance = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: { 'Content-Type': 'application/json' },
 });
--- components/create-blog/basic-info/CreateBlogInfo.tsx
+++ components/create-blog/basic-info/CreateBlogInfo.tsx
@@ -1,6 +1,11 @@
+'use client';
+import { useGetBlogInfo } from '@/hooks/blog';
+
 import CreateInfoLanding from './ui/CreateInfoLanding';
 
 const CreateBlogInfo = () => {
+  const data = useGetBlogInfo('Palms');
+  console.log(data);
   return <CreateInfoLanding />;
 };
 
