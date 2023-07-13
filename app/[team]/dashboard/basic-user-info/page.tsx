674848561765f92eaf1e197a5135424f83b45213
Geun-Oh <kandy1002@naver.com>
2023-07-14 02:54:30 +0900

    fix: 폴더 경로 수정사항 반영

--- app/[team]/dashboard/basic-user-info/page.tsx
+++ app/[team]/dashboard/basic-user-info/page.tsx
@@ -2,13 +2,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import DeleteButton from '@/components/dashboard/BasicUserInfo/UI/DeleteButton';
-import UserId from '@/components/dashboard/BasicUserInfo/UI/UserId';
-import UserInfoSaveButton from '@/components/dashboard/BasicUserInfo/UI/UserInfoSaveButton';
-import UserName from '@/components/dashboard/BasicUserInfo/UI/UserName';
-import UserOneLiner from '@/components/dashboard/BasicUserInfo/UI/UserOneLiner';
-import UserPosition from '@/components/dashboard/BasicUserInfo/UI/UserPosition';
-import UserProfile from '@/components/dashboard/BasicUserInfo/UI/UserProfile';
+import DeleteButton from '@/components/dashboard/BasicUserInfo/ui/DeleteButton';
+import UserId from '@/components/dashboard/BasicUserInfo/ui/UserId';
+import UserInfoSaveButton from '@/components/dashboard/BasicUserInfo/ui/UserInfoSaveButton';
+import UserName from '@/components/dashboard/BasicUserInfo/ui/UserName';
+import UserOneLiner from '@/components/dashboard/BasicUserInfo/ui/UserOneLiner';
+import UserPosition from '@/components/dashboard/BasicUserInfo/ui/UserPosition';
+import UserProfile from '@/components/dashboard/BasicUserInfo/ui/UserProfile';
 
 const BasicUserInfoPage = () => {
   return (
