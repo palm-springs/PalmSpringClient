7dfff53a7d411693fe4bd8318c1c7521faa1aed9
SynthiaLee <seanne0801@gmail.com>
2023-07-13 23:56:30 +0900

    feat/#83: 블로그 카테고리 별 페이지 생성

--- app/[team]/home/[category]/page.tsx
+++ app/[team]/home/[category]/page.tsx
@@ -1,8 +1,10 @@
 // 선택 카테고리 별 페이지
 import React from 'react';
 
+import ArticleContainer from '@/components/blog/UI/ArticleContainer';
+
 const CategoryPage = () => {
-  return <div></div>;
+  return <ArticleContainer />;
 };
 
 export default CategoryPage;
