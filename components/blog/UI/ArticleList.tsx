4d5377cf1f84edd7e64bf57a40f765443fea32ac
SynthiaLee <seanne0801@gmail.com>
2023-07-14 19:10:39 +0900

    feat/#83: 코드리뷰 소노클라우드 반영

--- components/blog/UI/ArticleList.tsx
+++ components/blog/UI/ArticleList.tsx
@@ -15,19 +15,19 @@ const ArticleList = () => {
       thumbnailImgUrl: 'https://unsplash.com/s/photos/image',
     },
     {
-      title: '리액트 API와 코드 재사용의 진화',
+      title: '인간의 생명 연장 가능성,유전자 편집 기술로 한 발 더 나아가다',
       description: '리액트 API와 코드 재사용의 진화에 관한 글입니다.',
       writer: '오형근 · 웹 프론트엔드 개발자',
       date: '2023.06.28',
     },
     {
-      title: '리액트 API와 코드 재사용의 진화에 대한 글의 제목이 길다면 어떨까아',
+      title: '바이오 연구의 첨단,인공 유전자로 인간 피부 재생 가능성',
       description: '리액트 API와 코드 재사용의 진화에 관한 글입니다.',
       writer: '길서현 · 웹 프론트엔드 개발자',
       date: '2023.06.27',
     },
     {
-      title: '리액트 API와 코드 재사용의 진화에 대한 글의 제목이 길다면 어떨까아아',
+      title: '무인 택배 드론 서비스,시험 운영 중으로 곧 상용화 예정',
       description: '이렇게 글 설명이 보입니다.',
       writer: '이시연 · 웹 프론트엔드 개발자',
       date: '2023.07.25',
