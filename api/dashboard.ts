eb4f4fc7bae4b77e1e338ed0f5f43ab3b1c60fb5
Geun-Oh <kandy1002@naver.com>
2023-07-14 21:04:21 +0900

    feat: 변경된 타입 적용

--- api/dashboard.ts
+++ api/dashboard.ts
@@ -1,48 +1,16 @@
 import { client } from '.';
 
-export const getPageList = async (
-  blogUrl: string,
-): Promise<{
-  code: number;
-  message: string;
-  data: Array<{
-    id: string;
-    title: string;
-    createdAt: string;
-    isDraft: boolean;
-  }>;
-}> => {
-  const { data } = await client.get(`/api/v1/page/${blogUrl}`);
+export const getPageList = async (blogUrl: string) => {
+  const { data } = await client.get<Response<PageListProps>>(`/api/v1/page/${blogUrl}`);
   return data;
 };
 
-export const getNavList = async (
-  blogUrl: string,
-): Promise<{
-  code: number;
-  message: string;
-  data: Array<{
-    id: string;
-    name: string;
-    navUrl: string;
-  }>;
-}> => {
-  const { data } = await client.get(`/api/v1/nav/${blogUrl}`);
+export const getNavList = async (blogUrl: string) => {
+  const { data } = await client.get<Response<NavListProps>>(`/api/v1/nav/${blogUrl}`);
   return data;
 };
 
-export const getCategoryList = async (
-  blogUrl: string,
-): Promise<{
-  code: number;
-  message: string;
-  data: Array<{
-    id: string;
-    name: string;
-    categoryUrl: string;
-    description: string;
-  }>;
-}> => {
-  const { data } = await client.get(`/api/v1/category/${blogUrl}`);
+export const getCategoryList = async (blogUrl: string) => {
+  const { data } = await client.get<Response<CategoryListProps>>(`/api/v1/category/${blogUrl}`);
   return data;
 };
--- hooks/useGetLastPathName.ts
+++ hooks/useGetLastPathName.ts
@@ -1,7 +1,5 @@
 import { usePathname } from 'next/navigation';
 
-import { dashBoardPageType } from '@/types/dashboard';
-
 const useGetLastPathName = (): dashBoardPageType => usePathname().split('/').pop() as dashBoardPageType;
 
 export default useGetLastPathName;
