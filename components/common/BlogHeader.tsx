a0e414c93cf1dd952b16223b6292e3548444a721
SynthiaLee <seanne0801@gmail.com>
2023-07-17 06:09:40 +0900

    feat/#90: header 상수 처리

--- components/common/BlogHeader.tsx
+++ components/common/BlogHeader.tsx
@@ -3,33 +3,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { BLOG_HEADER_EXAMPLE } from '@/constants/blogHeader';
+
 import BlogNav from './BlogNav';
 import HeaderLogo from './HeaderLogo';
 
 const BlogHeader = () => {
-  const BLOG_HEADER_EXAMPLE = {
-    logo: '',
-    blogName: 'Palms Blog',
-    navList: [
-      {
-        name: '피그마',
-        navUrl:
-          'https://www.figma.com/file/tZS8hE1DRyojfWWjY8XbrW/%EB%94%94%EC%9E%90%EC%9D%B4%EB%84%88-%EC%9E%91%EC%97%85%EA%B3%B5%EA%B0%84?type=design&node-id=1011-2021&mode=design&t=fd4v3Mc0hHiSMB5K-0',
-        isPage: false,
-      },
-      {
-        name: '문화',
-        navUrl: 'culture',
-        isPage: true,
-      },
-      {
-        name: '채용',
-        navUrl: 'recruit',
-        isPage: true,
-      },
-    ],
-  };
-
   return (
     <BlogHeaderContainer>
       <HeaderLogo logo={BLOG_HEADER_EXAMPLE.logo} blogName={BLOG_HEADER_EXAMPLE.blogName} />
