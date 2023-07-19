afe5d3d417b62d50c45089f4d4788572fc3789f5
Geun-Oh <kandy1002@naver.com>
2023-07-19 19:55:22 +0900

    feat: 블로그 설정 관련 put 메서드 추가

--- api/blog.ts
+++ api/blog.ts
@@ -11,12 +11,21 @@ export const getBlogInfo = async (blogUrl: string) => {
 export const getCheckBlogUrlDuplication = async (blogUrl: string) => {
   const {
     data: { data },
-  } = await client.get(`/blog/check?url=${blogUrl}`);
+  } = await client.get(`/api/v1/blog/check?url=${blogUrl}`);
   return data;
 };
 
 // 블로그 생성
 export const postCreateBlog = async (requestBody: createBlogData) => {
   const { data } = await client.post(`/api/v1/blog/create`, requestBody);
+  return data;
+};
+
+export const putBlogConfig = async (blogUrl: string, requestBody: any) => {
+  const { data } = await client.put(`/api/v1/blog/${blogUrl}/modify`, requestBody, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
   return data;
 };
