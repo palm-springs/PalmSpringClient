d6be4329c692677a1d9596b15c50b030193d33fb
SynthiaLee <seanne0801@gmail.com>
2023-07-20 04:46:54 +0900

    feat/#108: eslint 및 이미지 관련 config 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.ap-northeast-2.amazonaws.com',
+      },
+    ],
+  },
   reactStrictMode: true,
   webpack: (config) => {
     config.module.rules.push({
