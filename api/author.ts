b1acb1cc7c8e9fb15c80208a1ef992abcb9e43f7
Geun-Oh <kandy1002@naver.com>
2023-07-21 15:18:26 +0900

    fix: 수정사항 반영

--- api/author.ts
+++ api/author.ts
@@ -1,7 +1,7 @@
 import { AuthorInfoProps } from '@/types/author';
 import { Response } from '@/types/common';
 
-import { client } from '.';
+import client from '.';
 
 export const getAuthor = async (blogUrl: string, nickname: string) => {
   const { data } = await client.get<Response<AuthorInfoProps>>(`/api/v1/user/blog/${blogUrl}/${nickname}`);
