02b211c3d0067befd170c0a0ef422f50daac53c5
SynthiaLee <seanne0801@gmail.com>
2023-07-22 06:38:42 +0900

    chore/#138: 주석 삭제

--- components/blog/ui/ArticleContainer.tsx
+++ components/blog/ui/ArticleContainer.tsx
@@ -30,10 +30,7 @@ const ArticleContainer = (props: ArticleContainerProps) => {
   const { articleListData, thumbnail, description } = props;
   const FilteredCategoryList = useGetCategoryList(team);
 
-  console.log(articleListData);
-
   if (!FilteredCategoryList) return <LoadingLottie width={10} height={10} fit />;
-  // <div>로더</div>;
 
   const LiteralList = getLiteralCategoryList(FilteredCategoryList);
 
@@ -76,58 +73,6 @@ const ArticleContainer = (props: ArticleContainerProps) => {
         </ArticleWrapper>
       </>
     );
-
-  // if (!FilteredCategoryList) return <div>로더</div>;
-
-  // const LiteralList = getLiteralCategoryList(FilteredCategoryList);
-
-  // return (
-  //   <>
-  //     {/* //article list가 있을 때 - 블로그 대문이미지가 있을 때와 없을 때로 나뉨 */}
-  //     {contentListData ? (
-  //       thumbnail ? (
-  //         <>
-  //           <BlogImg thumbnail={thumbnail} description={description} />
-  //           <CategoryBtnWrapper>
-  //             <CategoryBtnBar LiteralList={LiteralList} />
-  //           </CategoryBtnWrapper>
-  //           <ArticleWrapper>
-  //             <ArticleList articleList={articleListData} />
-  //           </ArticleWrapper>
-  //         </>
-  //       ) : (
-  //         <>
-  //           <ContentInfoContainer>
-  //             {contentListData && contentListData.data.thumbnail && (
-  //               <Link href={`./content/${IndivContentId}`}>
-  //                 <Image src={BlogSampleImg} alt="blog thumbnail" />
-  //               </Link>
-  //               //실제 썸네일 url이 들어오면 위의 코드는 삭제 후 밑의 코드를 사용할 예정입니다!
-  //               // <Image src={CONTENT_INFO.thumbnail} alt="blog thumbnail" width={720} height={450} />
-  //             )}
-  //             <ContentInfo contentInfoData={contentListData.data} IndivContentId={IndivContentId} />
-  //           </ContentInfoContainer>
-  //           <CategoryBtnWrapper>
-  //             <CategoryBtnBar LiteralList={LiteralList} />
-  //           </CategoryBtnWrapper>
-  //           <ArticleWrapper>
-  //             <ArticleList articleList={articleListData} />
-  //           </ArticleWrapper>
-  //         </>
-  //       )
-  //     ) : thumbnail ? (
-  //       <BlogImgContainer>
-  //         {/* //article list가 없을 때 - 블로그 대문이미지가 있을 때와 없을 때로 나뉨 */}
-  //         <BlogImg thumbnail={thumbnail} description={description} />
-  //       </BlogImgContainer>
-  //     ) : (
-  //       <DefaultTextContainer>
-  //         <DefaultTitle>팜스프링 기술 블로그</DefaultTitle>
-  //         <DefaultSubText>등록된 글이 없습니다</DefaultSubText>
-  //       </DefaultTextContainer>
-  //     )}
-  //   </>
-  // );
 };
 
 export default ArticleContainer;
