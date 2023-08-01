e42e9d9fc464eaf09f7e6eaebe8d707a35568c9a
Geun-Oh <kandy1002@naver.com>
2023-08-02 00:22:48 +0900

    QA: 새 글 작성하기 onClick 라우팅 지정

--- components/dashboard/components/DashBoardNavBtn.tsx
+++ components/dashboard/components/DashBoardNavBtn.tsx
@@ -1,11 +1,20 @@
 'use client';
 
 import React from 'react';
+import { useParams, useRouter } from 'next/navigation';
 
 import DashBoardNavBtnContainer from './ui/DashBoardNavBtnContainer';
 
 const DashBoardNavBtn = () => {
-  return <DashBoardNavBtnContainer>새 글 작성하기</DashBoardNavBtnContainer>;
+  const { team: blogUrl } = useParams();
+
+  const router = useRouter();
+
+  return (
+    <DashBoardNavBtnContainer onDashboardClick={() => router.push(`/${blogUrl}/editor/page`)}>
+      새 글 작성하기
+    </DashBoardNavBtnContainer>
+  );
 };
 
 export default DashBoardNavBtn;
