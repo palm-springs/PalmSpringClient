b686ac024dfe019dc98d63ef3c010302e7acd61e
MyungJi_Jang <myungvely712@hufs.ac.kr>
2023-08-02 15:11:40 +0900

    feat/#159: 텍스트 에디터 스타일 분리 리팩토링

--- app/[team]/editor/page/page.tsx
+++ app/[team]/editor/page/page.tsx
@@ -4,23 +4,27 @@ import styled from 'styled-components';
 import AuthRequired from '@/components/auth/AuthRequired';
 import ArticleTitle from '@/components/editor/article/ui/ArticleTitle';
 import TextEditorBuild from '@/components/editor/TextEditorImport';
+import { TextEditorStyle } from '@/styles/TextEditorStyle';
 
 const CreatePagePage = () => {
   return (
     <AuthRequired>
-      <ArticleWrapper>
-        <ArticleTitle pageType="page" />
-        <TextEditorBuild pageType="page" />
-      </ArticleWrapper>
+      <TextEditorStyle>
+        <PageWrapper className="ProseMirror">
+          <ArticleTitle pageType="page" />
+          <TextEditorBuild pageType="page" />
+        </PageWrapper>
+      </TextEditorStyle>
     </AuthRequired>
   );
 };
 
 export default CreatePagePage;
 
-const ArticleWrapper = styled.div`
+const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  width: 100vw;
 `;
