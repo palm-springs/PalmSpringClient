86452f6edd06974bc393ec3a2e418eba21f670e9
SynthiaLee <seanne0801@gmail.com>
2023-08-16 20:27:50 +0900

    feat/#158: 대문이미지 및 콘텐츠 썸네일 짜부 해결

--- components/content/ui/PageTemplate.tsx
+++ components/content/ui/PageTemplate.tsx
@@ -73,9 +73,13 @@ const PageTemplate = (props: ContentTemplateProps) => {
 export default PageTemplate;
 
 const PageThumbnail = styled.img`
+  object-fit: cover;
   border-radius: 1.6rem;
   width: 72rem;
   height: 40.5rem;
+
+  user-select: none;
+  -webkit-user-drag: none;
 `;
 
 const ContentPageContainer = styled.section`
