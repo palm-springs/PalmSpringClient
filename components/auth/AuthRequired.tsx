282756a1a594415b8f9ea2d0bc5820af62f58ddf
seobbang <seobbangway@naver.com>
2023-08-16 17:05:41 +0900

    feat: refresh axios instance 생성, interceptor로 에러 처리

--- components/auth/AuthRequired.tsx
+++ components/auth/AuthRequired.tsx
@@ -4,7 +4,7 @@ import axios, { AxiosError } from 'axios';
 import { useRouter } from 'next/navigation';
 import { useResetRecoilState, useSetRecoilState } from 'recoil';
 
-import client from '@/api';
+import client, { refreshAxiosInstance } from '@/api';
 import { getRefreshToken } from '@/api/auth';
 
 import { accessTokenState } from './states/atom';
@@ -31,84 +31,32 @@ const AuthRequired = ({ children }: { children: React.ReactNode }) => {
   // access token 재발급 요청 함수
   const refresh = async () => {
     console.log('refresh');
-    const newRefreshData = await getRefreshToken();
+    const {
+      data: { accessToken },
+    } = await getRefreshToken();
 
-    if (newRefreshData) {
-      const {
-        status,
-        data: {
-          data: { accessToken },
-        },
-      } = newRefreshData;
-      console.log(status);
+    console.log(status);
 
-      switch (status) {
-        // refresh token 만료
-        case 401:
-          console.log('Refresh Token is expired.');
-          resetAccessToken();
-          sessionStorage?.removeItem('userToken');
-          return { status, newToken: null };
-        // access token 재발급 성공
-        case 200:
-          setAccessToken(accessToken);
-          console.log(`바꾸는거 : ${accessToken}`);
-          return { status, newToken: accessToken };
-        default:
-          break;
-      }
-    }
+    // switch (status) {
+    //   // refresh token 만료
+    //   case 401:
+    //   console.log('Refresh Token is expired.');
+    //   resetAccessToken();
+    //   sessionStorage?.removeItem('userToken');
+    //   return { status, newToken: null };
+    // // access token 재발급 성공
+    // case 200:
+    setAccessToken(accessToken);
+    console.log(`바꾸는거 : ${accessToken}`);
+    return accessToken;
+    // default:
+    //   break;
+    // }
   };
 
   useEffect(() => {
     console.log('here');
-    // axios.interceptors.request.use(async (config) => {
-    //   const accessToken = sessionStorage?.getItem('userToken');
-    //   console.log(accessToken);
-    //   console.log('request interceptor');
-    //   if (accessToken) {
-    //     console.log('헤더에 토큰 껴넣기~');
-    //     const { accessTokenState } = JSON.parse(accessToken);
-    //     config.headers.Authorization = `Bearer ${accessTokenState}`;
-    //     console.log(accessTokenState);
-    //   } else {
-    //     console.log('토큰 없다 안녕~');
-    //     router.push('/auth');
-    //   }
-
-    //   return config;
-    // });
     client.interceptors.response.use(
-      // async (response) => {
-      //   if (response.status >= 400) throw new Error(response.data);
-      //   const { config, status } = response;
-      //   const { code, message } = response.data;
-
-      //   console.log(response, status, code, message);
-
-      //   // access token 만료
-      //   if (status === 401) {
-      //     console.log('일단 여기에요');
-      //     router.push('/auth');
-      //     if (message === 'Access Token is expired.') {
-      //       await refresh();
-      //       console.log('여기까지 오지롱');
-      //       return client(config);
-      //     } else if (message === 'Refresh Token is expired.') {
-      //       resetAccessToken();
-      //       console.log('heeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeere');
-      //       router.push('/auth');
-      //     }
-      //   } else if (status === 400) {
-      //     sessionStorage?.removeItem('userToken');
-      //     router.push('/auth');
-      //   } else if (status === 403) {
-      //     sessionStorage?.removeItem('userToken');
-      //     router.push('/auth');
-      //   }
-
-      //   return response;
-      // },
       (response) => {
         console.log('response 성공');
         return response;
@@ -118,16 +66,30 @@ const AuthRequired = ({ children }: { children: React.ReactNode }) => {
         console.log(error);
 
         console.log('Access Token is expired.');
-        const refreshData = await refresh();
-        switch (refreshData?.status) {
-          case 200:
-            config.headers.Authorization = `Bearer ${refreshData?.newToken}`;
-            return client(config);
-          case 401:
-            router.push('/auth');
-            break;
-        }
+        const newAccessToken = await refresh();
+        // switch (refreshData?.status) {
+        //   case 200:
+        config.headers.Authorization = `Bearer ${newAccessToken}`;
+        return client(config);
+        //   case 401:
+        //     router.push('/auth');
+        //     break;
+        // }
 
+        return Promise.reject(error);
+      },
+    );
+
+    refreshAxiosInstance.interceptors.response.use(
+      (response) => {
+        console.log('refresh response 성공');
+        return response;
+      },
+      async (error) => {
+        console.log('Refresh Token is expired.');
+        resetAccessToken();
+        sessionStorage?.removeItem('userToken');
+        router.push('/auth');
         return Promise.reject(error);
       },
     );
