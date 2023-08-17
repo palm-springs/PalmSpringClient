4ea690022e96b152086f8aec8e5d267a34e702d4
Geun-Oh <kandy1002@naver.com>
2023-08-18 01:21:09 +0900

    feat: 임시저장된 글 삭제 및 라우팅 추가

---
+++ components/dashboard/tempsaved/IndivTempsavedContentList.tsx
@@ -0,0 +1,41 @@
+import { useParams, useRouter } from 'next/navigation';
+
+import { useDeleteArticle } from '@/hooks/dashboard';
+
+import DashBoardContent from '../components/DashBoardContent';
+
+interface IndivTempsavedContentListProps {
+  id: number;
+  title: string;
+  name: string;
+  job: string;
+  createdAt: string;
+}
+
+const IndivTempsavedContentList = (props: IndivTempsavedContentListProps) => {
+  const { team: blogUrl } = useParams();
+
+  const router = useRouter();
+
+  const { id, title, name, job, createdAt } = props;
+
+  const { mutate } = useDeleteArticle(blogUrl, id);
+
+  return (
+    <DashBoardContent
+      key={id}
+      id={String(id)}
+      content={title}
+      author={name}
+      position={job}
+      createdAt={createdAt}
+      onTitleClick={() => {
+        router.push(`/${blogUrl}/editor/article/edit/${id}`);
+      }}
+      onMutateClick={() => router.push(`/${blogUrl}/editor/article/edit/${id}`)}
+      onDeleteClick={() => mutate()}
+    />
+  );
+};
+
+export default IndivTempsavedContentList;
