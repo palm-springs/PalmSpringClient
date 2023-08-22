bf4571c8375ee56fc52203ecf34a8386f74918c9
seobbang <seobbangway@naver.com>
2023-08-22 20:55:34 +0900

    hotfix: 이미지 api path 수정

--- api/common.ts
+++ api/common.ts
@@ -1,7 +1,7 @@
 import client from '.';
 
 export const uploadImage = async (formData: FormData) => {
-  const { data } = await client.post(`/api/v1/image/add`, formData, {
+  const { data } = await client.post(`/api/v2/dashboard/image/add`, formData, {
     headers: { 'Content-Type': 'multipart/form-data' },
   });
   return data;
