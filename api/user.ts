f3ea298fde0c4a0c889562046b3d215be9dd27f4
seobbang <seobbangway@naver.com>
2023-08-24 04:26:01 +0900

    feat/#192: 팀 나가기, 팀 탈퇴하기 구현

--- api/user.ts
+++ api/user.ts
@@ -7,3 +7,13 @@ export const getCurrentUserInfo = async () => {
   const { data } = await client.get<Response<UserInfoProps>>(`/api/v1/user/dashboard/me/detail`);
   return data;
 };
+
+export const updateWithdrawTeam = async (blogUrl: string) => {
+  const { data } = await client.put<Response<UserInfoProps>>(`/api/v2/dashboard/user/me/out/${blogUrl}`);
+  return data;
+};
+
+export const updateWithdrawPlatform = async () => {
+  const { data } = await client.put<Response<string>>(`/api/v2/dashboard/user/me/out/register`);
+  return data;
+};
