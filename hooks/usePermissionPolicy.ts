653759f8d49b26e50d4bc40b458acb4e36c6d519
Geun-Oh <kandy1002@naver.com>
2023-10-02 02:08:29 +0900

    feat: 유저 정보를 가져와 permission을 생성하는 커스텀 훅 생성

---
+++ hooks/usePermissionPolicy.ts
@@ -0,0 +1,16 @@
+import { useRecoilValue } from 'recoil';
+
+import userRoleSelector from '@/recoil/selector/userRoleSelector';
+import PermissionPolicyChecker from '@/utils/PermissionPolicyClass';
+
+const usePerMissionPolicy = () => {
+  const userRole = useRecoilValue(userRoleSelector);
+
+  if (!userRole) throw new Error('유저가 없습니다. 다시 로그인해주세요!');
+
+  const UserPermissionPolicyChecker = PermissionPolicyChecker.getInstance(userRole);
+
+  return UserPermissionPolicyChecker;
+};
+
+export default usePerMissionPolicy;
