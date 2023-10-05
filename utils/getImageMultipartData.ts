3074a04bb99aaece0b7945ebd1cc2dbeefe92c65
MyungJi_Jang <myungvely712@hufs.ac.kr>
2023-10-05 15:09:22 +0900

    feat/#261: content image 새api 생성

--- utils/getImageMultipartData.ts
+++ utils/getImageMultipartData.ts
@@ -1,4 +1,4 @@
-import { uploadImage } from '@/api/common';
+import { uploadContentImage, uploadImage } from '@/api/common';
 
 //common -> get 어쩌구 불러오기
 export const getImageMultipartData = async (value: File) => {
@@ -6,5 +6,14 @@ export const getImageMultipartData = async (value: File) => {
   formData.append('image', value);
 
   const { data } = await uploadImage(formData);
+  return data;
+};
+
+//article, page 와 같은 content 이미지 저장하기
+export const getContentImageMultipartData = async (value: File, blogUrl: string) => {
+  const formData = new FormData();
+  formData.append('image', value);
+
+  const { data } = await uploadContentImage(blogUrl, formData);
   return data;
 };
