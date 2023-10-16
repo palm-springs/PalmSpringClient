89276d9a896611972fdd40c8849f7b69966eb545
MyungJi_Jang <myungvely712@hufs.ac.kr>
2023-10-16 09:42:15 +0900

    feat/#253: 새 글 작성하기 reset

--- components/dashboard/components/DashBoardNavBtn.tsx
+++ components/dashboard/components/DashBoardNavBtn.tsx
@@ -2,19 +2,24 @@
 
 import React from 'react';
 import { useParams, useRouter } from 'next/navigation';
+import { useResetRecoilState } from 'recoil';
+
+import { pageDataState } from '../../editor/states/atom';
 
 import DashBoardNavBtnContainer from './ui/DashBoardNavBtnContainer';
 
 const DashBoardNavBtn = () => {
   const { team: blogUrl } = useParams();
+  const resetPageData = useResetRecoilState(pageDataState);
 
   const router = useRouter();
 
-  return (
-    <DashBoardNavBtnContainer onDashboardClick={() => router.push(`/${blogUrl}/editor/page`)}>
-      새 글 작성하기
-    </DashBoardNavBtnContainer>
-  );
+  const handleOnNewPage = () => {
+    resetPageData();
+    router.push(`/${blogUrl}/editor/page`);
+  };
+
+  return <DashBoardNavBtnContainer onDashboardClick={handleOnNewPage}>새 글 작성하기</DashBoardNavBtnContainer>;
 };
 
 export default DashBoardNavBtn;
