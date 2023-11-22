cdbaf8e4425527494b4fc976492295a45ebaaf86
seobbang <seobbangway@naver.com>
2023-11-22 15:59:00 +0900

    feat/#299: 초대시 gmail 이메일 유효성 검증

--- components/dashboard/member/ui/MemberTemplate.tsx
+++ components/dashboard/member/ui/MemberTemplate.tsx
@@ -53,7 +53,7 @@ const MemberTemplate = () => {
             <DashboardCreateModal
               mainText="팀원 초대하기"
               buttonText="초대하기"
-              subText="쉼표, 엔터, 스페이스바로 메일 주소를 구분할 수 있습니다"
+              subText="쉼표, 엔터, 스페이스바로 메일 주소를 구분할 수 있습니다. &nbsp;&nbsp; 현재 Gmail 계정만 초대 가능합니다."
               buttonHandler={handleOnClickInvite}
               onModalCloseBtnClick={() => setModalState('')}
               disabled={emailDataList.length === 0 || isError}>
