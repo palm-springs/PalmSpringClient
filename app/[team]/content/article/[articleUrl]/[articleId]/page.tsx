dcd656623ebffbf8925db3a8cf858cfaa6aef42e
MyungJi_Jang <myungvely712@hufs.ac.kr>
2024-01-02 20:46:04 +0900

    chore/#314:오픈그래프 확인6

--- app/[team]/content/article/[articleUrl]/[articleId]/page.tsx
+++ app/[team]/content/article/[articleUrl]/[articleId]/page.tsx
@@ -1,11 +1,38 @@
 'use client';
 
 import React from 'react';
+import { Metadata } from 'next';
 import { useParams } from 'next/navigation';
 
+import { getBlogArticleList } from '@/api/blogHome';
 import LoadingLottie from '@/components/common/ui/LoadingLottie';
 import ArticleTemplate from '@/components/content/ui/ArticleTemplate';
 import { useGetBlogArticleDetail } from '@/hooks/blogHome';
+
+type Props = {
+  params: { team: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata | null> {
+  const team = params.team;
+  const product = await getBlogArticleList(team, '');
+
+  if (!product || product.code === 404) return null;
+
+  // const blogUrl = product.data.blogUrl;
+  const {
+    data: { title, description },
+  } = product;
+
+  return {
+    openGraph: {
+      title,
+      description,
+      type: 'website',
+    },
+  };
+}
 
 const ContentPage = () => {
   const { team, articleId } = useParams();
--- app/[team]/home/[category]/page.tsx
+++ app/[team]/home/[category]/page.tsx
@@ -1,36 +1,8 @@
 // 선택 카테고리 별 페이지
 import React from 'react';
-import { Metadata } from 'next';
 
-import { getSingleArticleData } from '@/api/article';
-import { getBlogInfo, getMetaBlogInfo } from '@/api/blog';
 import { getBlogArticleList, getBlogMainImg } from '@/api/blogHome';
 import ArticleContainer from '@/components/blog/ui/ArticleContainer';
-
-type Props = {
-  params: { team: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export async function generateMetadata({ params }: Props): Promise<Metadata | null> {
-  const team = params.team;
-  const product = await getBlogArticleList(team, '');
-
-  if (!product || product.code === 404) return null;
-
-  // const blogUrl = product.data.blogUrl;
-  const {
-    data: { title, description },
-  } = product;
-
-  return {
-    openGraph: {
-      title,
-      description,
-      // type: 'website',
-    },
-  };
-}
 
 const CategoryPage = async ({ params }: { params: { team: string } }) => {
   const { data } = await getBlogArticleList(params.team, '');
