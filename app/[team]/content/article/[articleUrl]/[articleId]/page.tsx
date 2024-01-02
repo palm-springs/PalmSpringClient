fc6f0051cb5216bc733a9f781e061f58e95f178e
MyungJi_Jang <myungvely712@hufs.ac.kr>
2024-01-02 20:56:19 +0900

    chore/#314:오픈그래프 확인9

--- app/[team]/content/article/[articleUrl]/[articleId]/page.tsx
+++ app/[team]/content/article/[articleUrl]/[articleId]/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from 'next';
 import { useParams } from 'next/navigation';
 
 import { getSingleArticleData } from '@/api/article';
-import { getBlogArticleList } from '@/api/blogHome';
+import { getBlogArticleDetail, getBlogArticleList } from '@/api/blogHome';
 import LoadingLottie from '@/components/common/ui/LoadingLottie';
 import ArticleTemplate from '@/components/content/ui/ArticleTemplate';
 import { useGetBlogArticleDetail } from '@/hooks/blogHome';
@@ -18,13 +18,13 @@ type Props = {
 export async function generateMetadata({ params }: Props): Promise<Metadata | null> {
   const team = params.team;
   const articleId = params.articleId;
-  const product = await getSingleArticleData(team, articleId);
+  const product = await getBlogArticleDetail(team, articleId);
 
   if (!product || product.code === 404) return null;
 
   // const blogUrl = product.data.blogUrl;
   const {
-    data: { title, description },
+    data: { title, content: description },
   } = product;
 
   return {
