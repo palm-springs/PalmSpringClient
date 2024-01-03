af370b5a6f5f2e3e576f12f0087ce07c3fe4faf0
seobbang <seobbangway@naver.com>
2024-01-03 18:40:51 +0900

    fix/#315: Line 스타일링 재수정

--- components/dashboard/components/ui/Line.tsx
+++ components/dashboard/components/ui/Line.tsx
@@ -12,6 +12,6 @@ export default Line;
 
 const LineUI = styled.hr<{ $isSideBar: boolean | undefined }>`
   margin: 0 0 0.8rem;
-  border: 1px solid ${({ theme, $isSideBar }) => ($isSideBar ? theme.colors.grey_300 : theme.colors.grey_400)};
-  width: 100%;
+  border: 1px solid ${({ theme }) => theme.colors.grey_300};
+  width: ${({ $isSideBar }) => ($isSideBar ? 'calc(100% - 3.2rem)' : '100%')};
 `;
