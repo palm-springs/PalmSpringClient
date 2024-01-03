9aa97ce32b7f4be6d661c2d44a150215d6f870d9
seobbang <seobbangway@naver.com>
2024-01-03 19:56:54 +0900

    feat/#315: 리다이렉트 설정

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,32 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export const middleware = (request: NextRequest) => {
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain
-  const subdomain = request.headers.get('host')?.split('.')[0]!;
+  const isSubdomain =
+    request.nextUrl.pathname.startsWith('/home') ||
+    request.nextUrl.pathname.startsWith('/content') ||
+    request.nextUrl.pathname.startsWith('/author');
 
-  return NextResponse.rewrite(new URL(`/${subdomain}${request.nextUrl.clone().pathname}`, request.url));
+  if (isSubdomain) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain
+    const subdomain = request.headers.get('host')?.split('.')[0]!;
+    return NextResponse.rewrite(new URL(`/${subdomain}${request.nextUrl.clone().pathname}`, request.url));
+  } else {
+    const pathName = request.nextUrl.clone().pathname;
+    const teamName = pathName.split('/')[1];
+    const index = pathName.indexOf('/', 1);
+    const targetPathName = pathName.slice(index);
+
+    return NextResponse.redirect(new URL(`https://${teamName}.palms.blog/${targetPathName}`, request.url));
+  }
 };
 
 export const config = {
-  matcher: ['/home/:path*', '/content/:path*', '/author/:path*'],
+  matcher: [
+    '/home/:path*',
+    '/content/:path*',
+    '/author/:path*',
+    '/:team/home/:path*',
+    '/:team/author/:path*',
+    '/:team/content/:path*',
+  ],
 };
