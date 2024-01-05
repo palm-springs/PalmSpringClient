af55ff534636bb0a0022b54aa21eef147598d65d
MyungJi_Jang <myungvely712@hufs.ac.kr>
2024-01-05 22:41:11 +0900

    chore/#323: opengraph-image.tsx파일 주석

--- app/[team]/home/opengraph-image.tsx
+++ app/[team]/home/opengraph-image.tsx
@@ -1,41 +1,41 @@
-import { ImageResponse } from 'next/server';
+// import { ImageResponse } from 'next/server';
 
-import { getMetaBlogInfo } from '@/api/blog';
+// import { getMetaBlogInfo } from '@/api/blog';
 
-export const alt = 'About Acme';
-export const size = {
-  width: 1200,
-  height: 630,
-};
-export const contentType = 'image/png';
+// export const alt = 'About Acme';
+// export const size = {
+//   width: 1200,
+//   height: 630,
+// };
+// export const contentType = 'image/png';
 
-export default function Image() {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <img
-          style={{
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-            width: '100%',
-            objectFit: 'cover',
-          }}
-          alt=""
-        />
-      </div>
-    ),
-    {
-      ...size,
-    },
-  );
-}
+// export default function Image() {
+//   return new ImageResponse(
+//     (
+//       <div
+//         style={{
+//           width: '100%',
+//           height: '100%',
+//           display: 'flex',
+//           alignItems: 'center',
+//           justifyContent: 'center',
+//         }}>
+//         <img
+//           style={{
+//             position: 'absolute',
+//             top: 0,
+//             right: 0,
+//             bottom: 0,
+//             left: 0,
+//             width: '100%',
+//             objectFit: 'cover',
+//           }}
+//           alt=""
+//         />
+//       </div>
+//     ),
+//     {
+//       ...size,
+//     },
+//   );
+// }
