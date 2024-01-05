2fdcd563bd3e8f8beaee244db0f5373f64018f8b
Geun-Oh <kandy1002@naver.com>
2024-01-05 13:56:08 +0100

    feat: createToast 유틸 수정 및 전체 토스트 생성 중복 로직 제거

--- utils/lib/toast.ts
+++ utils/lib/toast.ts
@@ -1,20 +1,58 @@
 import toast from 'react-hot-toast';
 
-export const createToast = (message: string, errorType: string) => {
+interface CommonToastProps {
+  type: 'ERROR' | 'NORMAL';
+  message: string;
+  id: string;
+  background?: string;
+}
+interface ErrorToastProps extends CommonToastProps {
+  type: 'ERROR';
+  duration?: number;
+}
+
+interface NormalToastProps extends CommonToastProps {
+  type: 'NORMAL';
+  duration?: number;
+}
+
+type ToastType = NormalToastProps | ErrorToastProps;
+
+const basicToastStyle = {
+  padding: '1.6rem 2rem',
+  borderRadius: '3.2rem',
+  color: '#fff',
+  fontSize: '1.4rem',
+  fontFamily: 'Pretendard',
+  fontStyle: 'normal',
+  fontWeight: '400',
+  letterSpacing: '-0.028rem',
+};
+
+export const createToast = (props: ToastType) => {
+  if (props.type === 'ERROR') {
+    const { message, id, duration, background } = props;
+    return () => {
+      toast.error(message, {
+        duration: duration,
+        id,
+        style: {
+          ...basicToastStyle,
+          background: background ?? '#343A40',
+        },
+      });
+    };
+  }
+
+  const { id, message, background, duration } = props;
+
   return () => {
-    toast.error(message, {
-      duration: 3000,
-      id: `error on ${errorType}`,
+    toast.success(message, {
+      duration,
+      id,
       style: {
-        padding: '1.6rem 2rem',
-        borderRadius: '3.2rem',
-        background: '#343A40',
-        color: '#fff',
-        fontSize: '1.4rem',
-        fontFamily: 'Pretendard',
-        fontStyle: 'normal',
-        fontWeight: '700',
-        letterSpacing: '-0.028rem',
+        ...basicToastStyle,
+        background: background ?? '#343A40',
       },
     });
   };
