c4e3d27ee267aa06ff2f89800ea5d60fe34a3ab0
seobbang <seobbangway@naver.com>
2024-01-07 19:59:20 +0900

    feat: 랜딩페이지 리다이렉트 재구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { redirect } from 'next/navigation';
 
 import client from '@/api';
@@ -5,30 +6,32 @@ import { getRefreshToken } from '@/api/auth';
 import { getUserInfoAfterLogin } from '@/api/dashboard';
 import LandingPage from '@/components/landing/LandingPage';
 
-import 'aos/dist/aos.css';
+const Home = () => {
+  const checkUser = async () => {
+    const data = await getRefreshToken();
+    console.log('HOME', data);
 
-const Home = async () => {
-  const data = await getRefreshToken();
-  console.log('HOME', data);
+    if (data.code === 201) {
+      console.log('1212줄임~');
+      const newAccessToken = data.data.accessToken;
+      if (newAccessToken) {
+        client.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
+        const { data } = await getUserInfoAfterLogin('', newAccessToken);
 
-  if (data.code === 201) {
-    console.log('1212줄임~');
-    const newAccessToken = data.data.accessToken;
-    if (newAccessToken) {
-      client.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
-      const { data } = await getUserInfoAfterLogin('', newAccessToken);
+        console.log(data);
 
-      console.log(data);
-
-      if (!data.joinBlogList || data.joinBlogList.length === 0) {
-        redirect(`/no-team/dashboard/upload`);
-      } else {
-        redirect(`/${data.joinBlogList[0].blogUrl}/dashboard/upload`);
+        if (!data.joinBlogList || data.joinBlogList.length === 0) {
+          redirect(`https://palms.blog/no-team/dashboard/upload`);
+        } else {
+          redirect(`https://palms.blog/${data.joinBlogList[0].blogUrl}/dashboard/upload`);
+        }
       }
     }
-  } else {
-    return <LandingPage />;
-  }
+  };
+
+  checkUser();
+
+  return <LandingPage />;
 };
 
 export default Home;
