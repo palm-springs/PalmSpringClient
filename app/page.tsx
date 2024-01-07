42e20545053777f19296e42336466b975e20b10f
seobbang <seobbangway@naver.com>
2024-01-07 21:15:46 +0900

    feat: 원상 복구

--- app/page.tsx
+++ app/page.tsx
@@ -7,31 +7,7 @@ import { getUserInfoAfterLogin } from '@/api/dashboard';
 import LandingPage from '@/components/landing/LandingPage';
 
 const Home = () => {
-  const checkUser = async () => {
-    const data = await getRefreshToken();
-    console.log('HOME', data);
-
-    if (data.code === 201) {
-      console.log('1212줄임~');
-      const newAccessToken = data.data.accessToken;
-      if (newAccessToken) {
-        client.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
-        const { data } = await getUserInfoAfterLogin('', newAccessToken);
-
-        console.log(data);
-
-        if (!data.joinBlogList || data.joinBlogList.length === 0) {
-          redirect(`https://palms.blog/no-team/dashboard/upload`);
-        } else {
-          redirect(`https://palms.blog/${data.joinBlogList[0].blogUrl}/dashboard/upload`);
-        }
-      }
-    } else {
-      return <LandingPage />;
-    }
-  };
-
-  checkUser();
+  return <LandingPage />;
 };
 
 export default Home;
