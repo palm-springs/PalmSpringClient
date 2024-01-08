77a321e1c3751ad1c5b6ab85f51ca9875aa4a670
seobbang <seobbangway@naver.com>
2024-01-09 00:59:50 +0900

    fix: no-team 분기 처리

--- components/no-team/TeamValidation.tsx
+++ components/no-team/TeamValidation.tsx
@@ -7,15 +7,13 @@ import { getUserInfo } from '@/api/dashboard';
 const TeamValidation = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
 
-  useEffect(() => {
-    const teamValidation = async () => {
-      const { data } = await getUserInfo();
-      if (data.joinBlogList.length > 0) {
-        router.push(`/${data.joinBlogList[0].blogUrl}/dashboard/upload`);
-      }
-    };
-    teamValidation();
-  }, []);
+  const teamValidation = async () => {
+    const { data } = await getUserInfo();
+    if (data.joinBlogList.length > 0) {
+      router.push(`/${data.joinBlogList[0].blogUrl}/dashboard/upload`);
+    }
+  };
+  teamValidation();
 
   return <div>{children}</div>;
 };
