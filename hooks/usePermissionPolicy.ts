f03584c8ab123a719680cd9670320dbc6a4c6405
seobbang <seobbangway@naver.com>
2024-01-09 04:00:00 +0900

    fix: team 변경시 유저 권한 재세팅 해결

--- hooks/usePermissionPolicy.ts
+++ hooks/usePermissionPolicy.ts
@@ -1,12 +1,28 @@
-import { useRecoilValue } from 'recoil';
+import { useEffect } from 'react';
+import { useParams } from 'next/navigation';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 import userState from '@/recoil/atom/user';
 import userRoleSelector from '@/recoil/selector/userRoleSelector';
 import PermissionPolicyChecker from '@/utils/PermissionPolicyClass';
 
 const usePerMissionPolicy = () => {
-  const userValue = useRecoilValue(userState);
-  console.log(userValue);
+  const { team } = useParams();
+  const [userValue, setUserValue] = useRecoilState(userState);
+
+  useEffect(() => {
+    const currentUserBlog = userValue?.joinBlogList.find(({ blogUrl }) => blogUrl === team);
+    if (userValue && currentUserBlog) {
+      setUserValue({
+        ...userValue,
+        currentUserRole: currentUserBlog.role,
+      });
+    }
+  }, [team]);
+
+  // const userValue = useRecoilValue(userState);
+
+  console.log(userValue, 'userPermission');
 
   if (!userValue?.currentUserRole) {
     throw new Error('유저가 없습니다. 다시 로그인해주세요!');
