3b68e611f375c1efca1235ba6c05f7d76856ced1
Geun-Oh <kandy1002@naver.com>
2024-02-03 06:11:27 +0900

    HOTFIX - v.1.1.1

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export const middleware = (request: NextRequest) => {
   const hostname = new URL(request.url).hostname;
   const subdomain = hostname.split('.')[0];
-  const isSubdomain = subdomain !== 'palms';
+  // const isSubdomain = subdomain !== 'palms';
   const pathWithoutAuthentication =
     request.nextUrl.pathname.startsWith('/home') ||
     request.nextUrl.pathname.startsWith('/content') ||
@@ -20,9 +20,11 @@ export const middleware = (request: NextRequest) => {
     const targetPathName = pathName.slice(index);
 
     // https://official.palms.blog 와 같이 해당 블로그인데 "/"로 접근하는 경우 "/home"으로 반환하도록 도와줍니다.
-    if (isSubdomain && pathName === '/') {
-      return NextResponse.redirect(new URL(`/home`, request.url));
-    }
+    // https://official.palms.blog/home 으로 리다이렉팅을 하고, 내용을 서브도메인에 맞게 rewrite해야합니다.
+    // if (isSubdomain && pathName === '/') {
+    //   const rewriteRes = NextResponse.rewrite(new URL(`/${subdomain}${request.nextUrl.clone().pathname}`, request.url));
+    //   return NextResponse.redirect(new URL(`/home`, rewriteRes.url));
+    // }
 
     // 이외에 기본적으로 다른 곳에서 흘러들어온 요청들을 모두 palms.blog 로 모아줍니다.
     // palms.blog와 palmsummer.site 분리를 위해서 root domain도 동적으로 설정
@@ -32,7 +34,7 @@ export const middleware = (request: NextRequest) => {
 
 export const config = {
   matcher: [
-    '/',
+    // '/',
     '/home/:path*',
     '/content/:path*',
     '/author/:path*',
