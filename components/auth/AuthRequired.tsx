4dff5f508c763552f112acf741668578e1d81f88
seobbang <seobbangway@naver.com>
2024-02-14 17:49:59 +0900

    refactor/#379: 로그인 유지 sessionStorage 이용하도록 변경

--- components/auth/AuthRequired.tsx
+++ components/auth/AuthRequired.tsx
@@ -1,13 +1,10 @@
 'use client';
 import { useEffect } from 'react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useRecoilState, useResetRecoilState } from 'recoil';
 
 import client, { refreshAxiosInstance } from '@/api';
 import { getRefreshToken } from '@/api/auth';
-import { LoginUserState } from '@/constants/Auth';
-
-import { accessTokenState } from './states/atom';
+import { ACCESS_TOKEN_KEY, LoginUserState } from '@/constants/Auth';
 
 // 로그인이 필요한 페이지에 대해 로그인 검사
 const AuthRequired = ({ children }: { children: React.ReactNode }) => {
@@ -19,18 +16,18 @@ const AuthRequired = ({ children }: { children: React.ReactNode }) => {
   // sessionStorage
   const sessionStorage = typeof window !== 'undefined' ? window.sessionStorage : undefined;
 
-  // recoil access token
-  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
-  const resetAccessToken = useResetRecoilState(accessTokenState);
+  // access token
+  const accessToken = sessionStorage?.getItem(ACCESS_TOKEN_KEY);
 
   // access token 재발급 요청 함수 (reissue)
   const refresh = async () => {
     const { code, data } = await getRefreshToken();
 
     if (code === 201) {
-      setAccessToken(data.accessToken);
+      sessionStorage?.setItem(ACCESS_TOKEN_KEY, data.accessToken);
       return data.accessToken;
     } else {
+      sessionStorage?.removeItem(ACCESS_TOKEN_KEY);
       return;
     }
   };
@@ -64,6 +61,7 @@ const AuthRequired = ({ children }: { children: React.ReactNode }) => {
           const newAccessToken = await refresh();
 
           config.headers.Authorization = `Bearer ${newAccessToken}`;
+          sessionStorage?.setItem(ACCESS_TOKEN_KEY, newAccessToken);
           return client(config);
         }
 
@@ -77,7 +75,7 @@ const AuthRequired = ({ children }: { children: React.ReactNode }) => {
         return response;
       },
       async (error) => {
-        resetAccessToken();
+        sessionStorage?.removeItem(ACCESS_TOKEN_KEY);
 
         const redirectUrl = pathname === '/invite' ? `${pathname}?code=${paramsCode}` : `${pathname}`;
         sessionStorage?.setItem('redirectUrl', redirectUrl);
--- components/auth/RequestAccessToken.ts
+++ components/auth/RequestAccessToken.ts
@@ -1,17 +1,13 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import { useSetRecoilState } from 'recoil';
 
 import { postSocialLogin } from '@/api/auth';
 import { getUserInfoAfterLogin } from '@/api/dashboard';
-import { LoginUserState } from '@/constants/Auth';
+import { ACCESS_TOKEN_KEY, LoginUserState } from '@/constants/Auth';
 import { useGetAccessToken } from '@/hooks/auth';
 import { getAccessTokenProps } from '@/types/auth';
 
-import { accessTokenState } from './states/atom';
-
 const RequestAccessToken = (props: getAccessTokenProps) => {
-  const setAccessToken = useSetRecoilState(accessTokenState);
   const platformData = useGetAccessToken(props);
 
   const sessionStorage = typeof window !== 'undefined' ? window.sessionStorage : undefined;
@@ -21,11 +17,14 @@ const RequestAccessToken = (props: getAccessTokenProps) => {
   const login = async () => {
     if (platformData) {
       const data = await postSocialLogin('google', platformData?.access_token);
-      if (!data) return;
+      if (!data) {
+        sessionStorage?.removeItem(ACCESS_TOKEN_KEY);
+        return;
+      }
 
       if (data.code === 200) {
         const { accessToken } = data.data;
-        setAccessToken(accessToken);
+        sessionStorage?.setItem(ACCESS_TOKEN_KEY, accessToken);
         if (redirectUrl) {
           sessionStorage?.removeItem('redirectUrl');
           router.replace(redirectUrl);
@@ -38,6 +37,7 @@ const RequestAccessToken = (props: getAccessTokenProps) => {
           }
         }
       } else {
+        sessionStorage?.removeItem(ACCESS_TOKEN_KEY);
         router.replace(`/auth?userState=${LoginUserState.WRONG_PLATFORM}`);
       }
     }
