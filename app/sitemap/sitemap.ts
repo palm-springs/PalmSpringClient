619c8b518f8c9f548f4ef2de9e086a4f2ef437cf
seobbang <seobbangway@naver.com>
2024-03-26 14:22:59 +0900

    test: 사이트맵 테스트

--- app/sitemap/sitemap.ts
+++ app/sitemap/sitemap.ts
@@ -1,16 +1,36 @@
 import { MetadataRoute } from 'next';
 
 import {
-  getBlogArticleList,
+  // getBlogArticleList,
   getBlogCategoryList,
   getBlogHeaderInfo,
-  getBlogListInfo,
+  // getBlogListInfo,
   getMemberListInfo,
 } from '@/api/blogHome';
 
+const getBlogListInfo = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v2/view/blog/site`);
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getBlogArticleList = async (blogUrl: string) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v2/view/article/${blogUrl}/list`);
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export async function generateSitemaps() {
   const { data: blogListInfo } = await getBlogListInfo();
-  return blogListInfo;
+  return blogListInfo.map(({ id }) => ({
+    id,
+  }));
 }
 
 export default async function sitemap({ id }: { id: number }): Promise<MetadataRoute.Sitemap> {
@@ -19,35 +39,35 @@ export default async function sitemap({ id }: { id: number }): Promise<MetadataR
   const team = blogListInfo.find(({ id: teamId }) => teamId === id)?.blogUrl;
 
   // 블로그별 정보 get
-  const { data: articles } = await getBlogArticleList(String(team), null);
-  const { data: pages } = await getBlogHeaderInfo(String(team));
-  const { data: categorys } = await getBlogCategoryList(String(team));
-  const { data: members } = await getMemberListInfo(String(team));
+  const { data: articles } = await getBlogArticleList(String(team));
+  // const { data: pages } = await getBlogHeaderInfo(String(team));
+  // const { data: categorys } = await getBlogCategoryList(String(team));
+  // const { data: members } = await getMemberListInfo(String(team));
 
   // 각 url 동적 생성
   const articleUrlList = articles.map(({ articleUrl }) => ({
     url: `https://${team}.${process.env.NEXT_PUBLIC_DOMAIN_NAME}/${articleUrl}`,
-    lastModified: new Date(),
+    lastModified: new Date().toISOString(),
   }));
 
-  const pageUrlList: { url: string; lastModified: Date }[] = [];
-  pages.navList.forEach(({ isPage, navUrl }) => {
-    if (isPage)
-      pageUrlList.push({
-        url: `https://${team}.${process.env.NEXT_PUBLIC_DOMAIN_NAME}/${navUrl}`,
-        lastModified: new Date(),
-      });
-  });
+  // const pageUrlList: { url: string; lastModified: string }[] = [];
+  // pages.navList.forEach(({ isPage, navUrl }) => {
+  //   if (isPage)
+  //     pageUrlList.push({
+  //       url: `https://${team}.${process.env.NEXT_PUBLIC_DOMAIN_NAME}/${navUrl}`,
+  //       lastModified: new Date().toISOString(),
+  //     });
+  // });
 
-  const categoryUrlList = categorys.map(({ categoryUrl }) => ({
-    url: `https://${team}.${process.env.NEXT_PUBLIC_DOMAIN_NAME}/category/${categoryUrl}`,
-    lastModified: new Date(),
-  }));
+  // const categoryUrlList = categorys.map(({ categoryUrl }) => ({
+  //   url: `https://${team}.${process.env.NEXT_PUBLIC_DOMAIN_NAME}/category/${categoryUrl}`,
+  //   lastModified: new Date().toISOString(),
+  // }));
 
-  const memberUrlList = members.map((nickname) => ({
-    url: `https://${team}.${process.env.NEXT_PUBLIC_DOMAIN_NAME}/author/${nickname}`,
-    lastModified: new Date(),
-  }));
+  // const memberUrlList = members.map((nickname) => ({
+  //   url: `https://${team}.${process.env.NEXT_PUBLIC_DOMAIN_NAME}/author/${nickname}`,
+  //   lastModified: new Date().toISOString(),
+  // }));
 
   // 사이트맵 url return
   return [
@@ -57,8 +77,8 @@ export default async function sitemap({ id }: { id: number }): Promise<MetadataR
       priority: 1,
     },
     ...articleUrlList,
-    ...pageUrlList,
-    ...categoryUrlList,
-    ...memberUrlList,
+    // ...pageUrlList,
+    // ...categoryUrlList,
+    // ...memberUrlList,
   ];
 }
