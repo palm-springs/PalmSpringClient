8d9392c8cabe7ed778335b92da66b72d9004c67d
MyungJi_Jang <myungvely712@hufs.ac.kr>
2024-04-09 20:58:07 +0900

    fix/#423: 리뷰반영

--- components/editor/publish/ui/CategorySelect.tsx
+++ components/editor/publish/ui/CategorySelect.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
 
 import LoadingLottie from '@/components/common/ui/LoadingLottie';
