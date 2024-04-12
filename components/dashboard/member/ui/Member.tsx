f042e259065bc18ae6e18e9ee0403f5f4c761a8f
Geun-Oh <kandy1002@naver.com>
2024-04-12 18:53:08 +0900

    HOTFIX - v3.4.7
    fix: only owner can change user role
    
    fix: only owner can change user role

--- components/dashboard/member/ui/Member.tsx
+++ components/dashboard/member/ui/Member.tsx
@@ -12,9 +12,7 @@ import {
   CharmMenuMeatballIcon,
   IcClose24Icon,
   IcUserIcon,
-  NoUserProfileIcon,
 } from '@/public/icons';
-import { MemberExampleImg } from '@/public/images';
 import { RoleType } from '@/utils/PermissionPolicyClass';
 
 import MemberPermissionPopOver from '../MemberPermissionPopOver';
@@ -124,7 +122,7 @@ const Member = (props: MemberComponentProps) => {
                 }}>
                 <Role>
                   {memberRole} {isUserCanEditIndivMemberPermission && <ArrowDownSmallIcon />}
-                  {isPermissionModalOpen === email && (
+                  {isPermissionModalOpen === email && isUserCanEditIndivMemberPermission && (
                     <MemberPermissionPopOver memberRole={role} memberId={memberId} memberEmail={email} />
                   )}
                 </Role>
