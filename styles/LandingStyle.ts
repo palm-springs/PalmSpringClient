6bb4006ccd1c57f9ea2e7c0f7948fd4869e14f6b
seobbang <seobbangway@naver.com>
2024-04-26 13:59:52 +0900

    RELEASE - v3.6.1 - hotfix : font 문제 수정

--- styles/LandingStyle.ts
+++ styles/LandingStyle.ts
@@ -2,6 +2,10 @@ import { createGlobalStyle } from 'styled-components';
 
 export const LandingStyle = createGlobalStyle`
 
+@import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css');
+@import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
+
 * {
     box-sizing: border-box;
     margin: 0;
