c2ac1f3d43e818fe5a36574e1041c491742102ee
seobbang <seobbangway@naver.com>
2024-06-01 22:08:45 +0900

    fix/#468: cdn 주소 get api 병렬 요청 처리

--- utils/editor/getEditorContent.ts
+++ utils/editor/getEditorContent.ts
@@ -2,19 +2,26 @@ import { getContentCtrlVImage } from '../getImageMultipartData';
 
 // 이미지 CDN 주소로 바꿔 끼우기 함수
 const changeImgSrc = async (team: string) => {
-  const imgList = document.querySelectorAll('img:not([src^="https://cdn.palms.blog/"]):not([class])');
-  const tempImgArray: string[] = [];
+  const imgList = document.querySelectorAll(
+    'img:not([src^="https://cdn.palms.blog/"]):not([class="ProseMirror-separator"])',
+  );
 
-  for (const img of imgList) {
-    const imgSrc = img?.getAttribute('src');
+  const promises = Array.from(imgList).map(async (img) => {
+    const imgSrc = img.getAttribute('src');
     if (!imgSrc) return;
 
     const blob = await fetch(imgSrc).then((res) => res.blob());
     const file = new File([blob], 'image.png', { type: 'image/png' });
     const imgUrl = await getContentCtrlVImage(file, String(team));
-    tempImgArray.push(imgUrl);
-    img?.setAttribute('src', imgUrl);
-  }
+    img.setAttribute('src', imgUrl);
+
+    return imgUrl;
+  });
+
+  const results = await Promise.allSettled(promises);
+  const tempImgArray = results
+    .filter((result) => result.status === 'fulfilled')
+    .map((result) => (result as PromiseFulfilledResult<string>).value);
 
   return tempImgArray;
 };
