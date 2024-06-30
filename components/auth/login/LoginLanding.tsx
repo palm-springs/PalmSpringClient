da555e99a81b81b965a1407ec6a53c4b6d1bd4f5
9911120 <psalm1266@gmail.com>
2024-07-01 02:51:00 +0900

    feat/#500: 구글로그인 먹통 대응 공지문구 추가

--- components/auth/login/LoginLanding.tsx
+++ components/auth/login/LoginLanding.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from 'react';
 import { Toaster } from 'react-hot-toast';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
+import styled from 'styled-components';
 
 import { platformLogin } from '@/api/auth';
 import GoogleLoginLanding from '@/components/auth/login/GoogleLoginLanding';
@@ -90,9 +91,28 @@ const LoginLanding = () => {
           로그인
         </BgButton>
         <LinkButton href="/sign-up">회원가입</LinkButton>
+        <NoticeInfoContainer>
+          기존 Google 가입 계정에 대해 Google 로그인이 원활하게 진행되지 않는 경우,{' '}
+          <Link href="/login/password">비밀번호 찾기</Link> 를 통해 비밀번호를 설정하신 후 이메일/비밀번호를 통해서도
+          로그인할 수 있습니다.
+        </NoticeInfoContainer>
       </FlexContainer>
     </>
   );
 };
 
 export default LoginLanding;
+
+const NoticeInfoContainer = styled.div`
+  margin-top: 4.8rem;
+  line-height: 1.6;
+  color: ${({ theme }) => theme.colors.grey_600};
+  font-size: 1.4rem;
+  & a {
+    border-radius: 6px;
+    background: ${({ theme }) => theme.colors.background_green_soft};
+    padding: 0.2rem 0.6rem;
+    color: ${({ theme }) => theme.colors.green_hover};
+    font-size: 1.2rem;
+  }
+`;
