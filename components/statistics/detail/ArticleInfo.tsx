6f5aab8a8982f84e564c0fa826bab0a5ef2b1eb7
MyungJi_Jang <myungvely712@hufs.ac.kr>
2024-07-07 01:04:43 +0900

    feat/#479: 게시글 통계추가

--- components/statistics/detail/ArticleInfo.tsx
+++ components/statistics/detail/ArticleInfo.tsx
@@ -1,27 +1,53 @@
 'use client';
 import Image from 'next/image';
 import styled from 'styled-components';
+import VisitantUI from '../BlogStatistics/VisitantUI';
 
 const ArticleInfo = () => {
+  const today = new Date();
+  const formattedDate = `${today.getFullYear()}.${('0' + (today.getMonth() + 1)).slice(-2)}.${('0' + today.getDate()).slice(-2)} ${('0' + today.getHours()).slice(-2)}:${('0' + today.getMinutes()).slice(-2)}`;
+
   return (
-    <ArticleInfoContainer>
-      <Thumbnail />
-      <div>
-        <Title>리액트 API와 코드 재사용의 진화에 대한 글의 제목이 길다면 어떨까</Title>
-        <Description>리액트 API와 코드 재사용의 진화에 관한 글입니다.</Description>
-        <AuthorAndDate>송승훈 | 2023.06.25</AuthorAndDate>
-      </div>
-    </ArticleInfoContainer>
+    <>
+      <ArticleInfoContainer>
+        <Thumbnail />
+        <div>
+          <Title>리액트 API와 코드 재사용의 진화에 대한 글의 제목이 길다면 어떨까</Title>
+          <Description>리액트 API와 코드 재사용의 진화에 관한 글입니다.</Description>
+          <AuthorAndDate>송승훈 | 2023.06.25</AuthorAndDate>
+        </div>
+      </ArticleInfoContainer>
+      <ArticleStatisticsWrapper>
+        <ArticleStatisticsTitle>해당 게시글 통계</ArticleStatisticsTitle>
+        <ArticleStatisticsTime>{formattedDate}</ArticleStatisticsTime>
+      </ArticleStatisticsWrapper>
+      <VisitantUI />
+    </>
   );
 };
 
 export default ArticleInfo;
 
+const ArticleStatisticsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 4rem 0 0 4rem;
+`;
+
+const ArticleStatisticsTime = styled.p`
+  margin: 0.4rem 0 0 0.8rem;
+  ${({ theme }) => theme.fonts.Caption};
+  color: ${({ theme }) => theme.colors.grey_700};
+`;
+
+const ArticleStatisticsTitle = styled.h2`
+  ${({ theme }) => theme.fonts.Body2_Semibold};
+  color: ${({ theme }) => theme.colors.grey_900};
+`;
+
 const ArticleInfoContainer = styled.div`
   display: flex;
-
   gap: 3.2rem;
-
   margin-top: 2.4rem;
   padding: 0 4rem;
 `;
